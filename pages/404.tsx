0599bfcd8f4368c2b212f34ac905ead4b1a9d311
Nicholas Cannon <nicholascannon1@gmail.com>
2021-09-25 11:59:31 +0800

    404 page

---
+++ pages/404.tsx
@@ -0,0 +1,27 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import Link from "next/link";
+import styles from "../styles/404.module.css";
+
+const NotFound: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>niccannon.com - Not Found</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.container}>
+        <h2>Page not found</h2>
+        <p>
+          Go back{" "}
+          <Link href="/">
+            <a className={styles.link}>home</a>
+          </Link>
+        </p>
+      </main>
+    </>
+  );
+};
+
+export default NotFound;
