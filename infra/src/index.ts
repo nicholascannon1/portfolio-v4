13080933c3d2a142f1502f40e456175ec564a954
Nicholas Cannon <nicholascannon1@gmail.com>
2022-06-27 21:15:06 +0800

    run prettier over everything

--- infra/src/index.ts
+++ infra/src/index.ts
@@ -1,7 +1,7 @@
-import * as pulumi from "@pulumi/pulumi";
-import * as aws from "@pulumi/aws";
-import * as path from "path";
-import { syncWebsiteFiles } from "./s3-utils";
+import * as pulumi from '@pulumi/pulumi';
+import * as aws from '@pulumi/aws';
+import * as path from 'path';
+import { syncWebsiteFiles } from './s3-utils';
 
 const BUILD_DIR = '../../client/out';
 
@@ -9,21 +9,21 @@ const config = new pulumi.Config();
 const stack = pulumi.getStack();
 const prefix = `${pulumi.getProject()}-${stack}`;
 
-const certStack = new pulumi.StackReference("nicholascannon1/portfolio-cert/prod");
+const certStack = new pulumi.StackReference('nicholascannon1/portfolio-cert/prod');
 
-const zoneId = config.require("hosted-zone-id");
-const recordName = config.require("record-name");
+const zoneId = config.require('hosted-zone-id');
+const recordName = config.require('record-name');
 const cnameRecordName = `www.${recordName}`;
-const certArn = certStack.getOutput("certArn");
+const certArn = certStack.getOutput('certArn');
 const tags = { project: prefix };
 
 const bucket = new aws.s3.Bucket(`${prefix}-bucket`, {
-  acl: "private",
-  website: {
-    indexDocument: "index.html",
-    errorDocument: "index.html",
-  },
-  tags,
+    acl: 'private',
+    website: {
+        indexDocument: 'index.html',
+        errorDocument: 'index.html',
+    },
+    tags,
 });
 
 syncWebsiteFiles(path.join(__dirname, BUILD_DIR), bucket, tags);
@@ -31,89 +31,89 @@ syncWebsiteFiles(path.join(__dirname, BUILD_DIR), bucket, tags);
 const originAccessIdentity = new aws.cloudfront.OriginAccessIdentity(`${prefix}-oai`);
 
 const bucketPolicy = new aws.s3.BucketPolicy(`${prefix}-bucket-policy`, {
-  bucket: bucket.id,
-  policy: pulumi.all([bucket.bucket, originAccessIdentity.iamArn]).apply(([bucketName, oiaArn]) =>
-    JSON.stringify({
-      Version: "2012-10-17",
-      Statement: [
-        {
-          Effect: "Allow",
-          Principal: {
-            AWS: oiaArn,
-          },
-          Action: ["s3:GetObject"],
-          Resource: [`arn:aws:s3:::${bucketName}/*`],
-        },
-      ],
-    })
-  ),
+    bucket: bucket.id,
+    policy: pulumi.all([bucket.bucket, originAccessIdentity.iamArn]).apply(([bucketName, oiaArn]) =>
+        JSON.stringify({
+            Version: '2012-10-17',
+            Statement: [
+                {
+                    Effect: 'Allow',
+                    Principal: {
+                        AWS: oiaArn,
+                    },
+                    Action: ['s3:GetObject'],
+                    Resource: [`arn:aws:s3:::${bucketName}/*`],
+                },
+            ],
+        })
+    ),
 });
 
-const CachingOptimizedPolicyId = "658327ea-f89d-4fab-a63d-7e88639e58f6";
+const CachingOptimizedPolicyId = '658327ea-f89d-4fab-a63d-7e88639e58f6';
 
 const distribution = new aws.cloudfront.Distribution(`portolio-distribution-${stack}`, {
-  origins: [
-    {
-      domainName: bucket.bucketRegionalDomainName,
-      s3OriginConfig: {
-        originAccessIdentity: originAccessIdentity.cloudfrontAccessIdentityPath,
-      },
-      originId: "portfolio-website",
+    origins: [
+        {
+            domainName: bucket.bucketRegionalDomainName,
+            s3OriginConfig: {
+                originAccessIdentity: originAccessIdentity.cloudfrontAccessIdentityPath,
+            },
+            originId: 'portfolio-website',
+        },
+    ],
+    enabled: true,
+    defaultRootObject: 'index.html',
+    aliases: [recordName, cnameRecordName],
+    defaultCacheBehavior: {
+        allowedMethods: ['GET', 'HEAD', 'OPTIONS'],
+        cachedMethods: ['GET', 'HEAD'],
+        targetOriginId: 'portfolio-website',
+        viewerProtocolPolicy: 'redirect-to-https',
+        cachePolicyId: CachingOptimizedPolicyId,
     },
-  ],
-  enabled: true,
-  defaultRootObject: "index.html",
-  aliases: [recordName, cnameRecordName],
-  defaultCacheBehavior: {
-    allowedMethods: ["GET", "HEAD", "OPTIONS"],
-    cachedMethods: ["GET", "HEAD"],
-    targetOriginId: "portfolio-website",
-    viewerProtocolPolicy: "redirect-to-https",
-    cachePolicyId: CachingOptimizedPolicyId,
-  },
-  customErrorResponses: [
-    {
-      errorCode: 404,
-      responseCode: 404,
-      responsePagePath: "/index.html",
+    customErrorResponses: [
+        {
+            errorCode: 404,
+            responseCode: 404,
+            responsePagePath: '/index.html',
+        },
+        {
+            errorCode: 403,
+            responseCode: 200,
+            responsePagePath: '/index.html',
+        },
+    ],
+    restrictions: {
+        geoRestriction: {
+            restrictionType: 'none',
+        },
     },
-    {
-      errorCode: 403,
-      responseCode: 200,
-      responsePagePath: "/index.html",
+    viewerCertificate: {
+        acmCertificateArn: certArn,
+        sslSupportMethod: 'sni-only',
     },
-  ],
-  restrictions: {
-    geoRestriction: {
-      restrictionType: "none",
-    },
-  },
-  viewerCertificate: {
-    acmCertificateArn: certArn,
-    sslSupportMethod: "sni-only",
-  },
-  tags,
+    tags,
 });
 
 const record = new aws.route53.Record(`portfolio-record-home-a-${stack}`, {
-  zoneId,
-  name: recordName,
-  type: "A",
-  aliases: [
-    {
-      evaluateTargetHealth: false,
-      name: distribution.domainName,
-      zoneId: distribution.hostedZoneId,
-    },
-  ],
+    zoneId,
+    name: recordName,
+    type: 'A',
+    aliases: [
+        {
+            evaluateTargetHealth: false,
+            name: distribution.domainName,
+            zoneId: distribution.hostedZoneId,
+        },
+    ],
 });
 
 const cname = new aws.route53.Record(`portfolio-record-home-cname-${stack}`, {
-  zoneId,
-  name: cnameRecordName,
-  type: "CNAME",
-  ttl: 300,
-  records: [recordName],
+    zoneId,
+    name: cnameRecordName,
+    type: 'CNAME',
+    ttl: 300,
+    records: [recordName],
 });
 
 export const bucketName = bucket.bucket;
--- infra/src/s3-utils.ts
+++ infra/src/s3-utils.ts
@@ -9,29 +9,29 @@ const IGNORED_FILES = ['.DS_Store', '.env'];
 const isDirectory = (filePath: string): boolean => fs.lstatSync(filePath).isDirectory();
 
 export const syncWebsiteFiles = (
-  directoryPath: string,
-  bucket: aws.s3.Bucket,
-  tags?: { [key: string]: string },
-  folder?: string
+    directoryPath: string,
+    bucket: aws.s3.Bucket,
+    tags?: { [key: string]: string },
+    folder?: string
 ) => {
-  for (let file of fs.readdirSync(directoryPath)) {
-    if (IGNORED_FILES.includes(file)) continue;
+    for (let file of fs.readdirSync(directoryPath)) {
+        if (IGNORED_FILES.includes(file)) continue;
 
-    const filePath = path.join(directoryPath, file);
-    if (isDirectory(filePath)) {
-      // recurse on the dir and sync files with prefix
-      const folderPath = folder === undefined ? file : path.join(folder, file);
-      syncWebsiteFiles(filePath, bucket, tags, folderPath);
-    } else {
-      const fileKey = folder ? `${folder}/${file}` : file;
-      new aws.s3.BucketObject(fileKey, {
-        bucket: bucket,
-        key: fileKey,
-        source: new pulumi.asset.FileAsset(filePath),
-        contentType: mime.getType(filePath) || undefined,
-        cacheControl: file === 'index.html' ? 'no-store' : 'public', // never cache index.html
-        tags,
-      });
+        const filePath = path.join(directoryPath, file);
+        if (isDirectory(filePath)) {
+            // recurse on the dir and sync files with prefix
+            const folderPath = folder === undefined ? file : path.join(folder, file);
+            syncWebsiteFiles(filePath, bucket, tags, folderPath);
+        } else {
+            const fileKey = folder ? `${folder}/${file}` : file;
+            new aws.s3.BucketObject(fileKey, {
+                bucket: bucket,
+                key: fileKey,
+                source: new pulumi.asset.FileAsset(filePath),
+                contentType: mime.getType(filePath) || undefined,
+                cacheControl: file === 'index.html' ? 'no-store' : 'public', // never cache index.html
+                tags,
+            });
+        }
     }
-  }
 };
