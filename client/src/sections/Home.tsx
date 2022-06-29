08e3779290c259570e224d63e6eac230f5a17a86
Nicholas Cannon <nicholascannon1@gmail.com>
2022-06-29 17:56:45 +0800

    Put home text under photo

--- client/src/sections/Home.tsx
+++ client/src/sections/Home.tsx
@@ -5,13 +5,13 @@ import styles from '../../styles/home.module.css';
 const HomeSection: React.FC = () => {
     return (
         <Section className={styles.container}>
+            <img className={styles.profilePic} src="/imgs/profilePic.jpg" alt="profile picture" />
+
             <div className={styles.titleBox}>
                 <h1>Nicholas Cannon</h1>
                 <p>Full Stack Software Developer</p>
                 <span>Perth, Western Australia</span>
             </div>
-
-            <img className={styles.profilePic} src="/imgs/profilePic.jpg" alt="profile picture" />
         </Section>
     );
 };
