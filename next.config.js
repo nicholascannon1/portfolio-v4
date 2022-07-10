f08fae32eb09fc6df83394c4fd1ceb59196e016a
Nicholas Cannon <nicholascannon1@gmail.com>
2022-07-10 11:51:43 +0800

    Dynamically generate sitemap

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
     reactStrictMode: true,
+    webpack: (config, { isServer }) => {
+        // generate sitemap.xml when webpack is run serverside
+        if (isServer) {
+            import('./scripts/generate-sitemap.mjs');
+        }
+
+        return config;
+    },
 };
---
+++ scripts/generate-sitemap.mjs
@@ -0,0 +1,37 @@
+import * as fs from 'fs';
+import { globbySync } from 'globby';
+
+const WEBSITE_URL = 'nicholascannon.com';
+
+/**
+ *
+ * @param {String} page
+ */
+const getPageXml = (page) => {
+    const pagePath = page.replace('src/pages', '').replace('.tsx', '').replace('.mdx', '');
+    const route = pagePath === '/index' ? '' : pagePath;
+
+    return `
+        <url>
+            <loc>${WEBSITE_URL}${route}</loc>
+            <lastmod>${new Date().toISOString()}</lastmod>
+            <changefreq>monthly</changefreq>
+            <priority>1.0</priority>
+        </url>
+    `.trim();
+};
+
+const generateSitemap = () => {
+    const pages = globbySync(['src/pages/**/*{.tsx, .mdx}', '!src/pages/_*.tsx', '!src/pages/api']);
+    const sitemap = `
+        <?xml version="1.0" encoding="UTF-8"?>
+        <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+            ${pages.map((page) => getPageXml(page)).join('\n')}
+        </urlset>
+    `.trim();
+    fs.writeFileSync('public/sitemap.xml', sitemap);
+
+    console.log('info  - Sitemap generated!');
+};
+
+generateSitemap();
