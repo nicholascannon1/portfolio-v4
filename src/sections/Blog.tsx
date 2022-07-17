c4bb577aa56f6e6585f9818b6a6769e61745b695
Nicholas Cannon <nicholascannon1@gmail.com>
2022-07-17 16:18:11 +0800

    Tech blog index section

--- src/sections/Blog.tsx
+++ src/sections/Blog.tsx
@@ -2,6 +2,7 @@ import Link from 'next/link';
 import React from 'react';
 import Grid from '../components/Grid';
 import Section from '../components/Section';
+import styles from '../../styles/blog.module.css';
 
 type Post = {
     slug: string;
@@ -14,18 +15,19 @@ type Props = {
 
 const BlogSection: React.FC<Props> = ({ posts }) => {
     return (
-        <Section title="Technical Blog">
-            <h3>subtext here</h3>
-
+        <Section className={styles.container} title="Technical Blog">
             <Grid maxColWidth={4} shrinkToSingleColumn={true}>
                 {posts.map(({ slug, frontmatter }) => (
-                    // TODO: finish blog component
-                    <div key={slug}>
-                        <Link passHref href={`posts/${slug}`}>
-                            <h4>{frontmatter.title}</h4>
-                            {/* img here */}
-                        </Link>
-                    </div>
+                    <Link key={slug} passHref href={`posts/${slug}`}>
+                        <a className={styles.blog}>
+                            <img src={frontmatter.image} alt={`${frontmatter.title} image`} />
+                            <div>
+                                <h4>{frontmatter.title}</h4>
+                                <p>{frontmatter.description}</p>
+                                <p>{frontmatter.date}</p>
+                            </div>
+                        </a>
+                    </Link>
                 ))}
             </Grid>
         </Section>
