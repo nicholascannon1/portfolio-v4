a15e327653c552e599cb128f3e2787f53fe89f8d
nicholascannon <nicholascannon1@gmail.com>
2024-03-19 13:59:54 +1100

    Upgrade everything!!!

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,7 +7,7 @@ class MyDocument extends Document {
                 <Head>
                     <link rel="icon" href="/favicon.ico" />
                     <link rel="preconnect" href="https://fonts.googleapis.com" />
-                    <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+                    <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
                     <link
                         href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap"
                         rel="stylesheet"
