e4e87731101a293bb0ce8d975fb00f35c53bae27
Nicholas Cannon <nicholascannon1@gmail.com>
2024-03-19 14:58:08 +1100

    2024 Refresh (#20)
    * Reset eslint rules
    
    * uninstall unneeded stuff
    
    * Update package.json
    
    * Docs update
    
    * Upgrade node to v18
    
    * Upgrade node to v18.17.0 for next14
    
    * Upgrade everything!!!
    
    * Use Image component
    
    * Setup for deploy on cloudflare pages
    
    * Update to experimental edge
    
    * Remove google fonts add fontsource font

--- src/components/SocialHud.tsx
+++ src/components/SocialHud.tsx
@@ -1,14 +1,16 @@
+import Image from 'next/image';
+
 export const SocialHud = () => {
     return (
         <div className="hud">
             <a href="mailto:nicholas.cannon1@gmail.com">
-                <img src="/imgs/socials/email.svg" alt="email" />
+                <Image src="/images/socials/email.svg" width={24} height={24} alt="email" />
             </a>
             <a href="https://www.linkedin.com/in/niccannon1" target="_blank" rel="noopener noreferrer">
-                <img src="/imgs/socials/linkedin.svg" alt="linkedin" />
+                <Image src="/images/socials/linkedin.svg" width={24} height={24} alt="linkedin" />
             </a>
             <a href="https://github.com/nicholascannon" target="_blank" rel="noopener noreferrer">
-                <img src="/imgs/socials/github.svg" alt="github" />
+                <Image src="/images/socials/github.svg" width={24} height={24} alt="github" />
             </a>
         </div>
     );
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
-import type { AppProps } from 'next/app';
+import '@fontsource-variable/montserrat';
 import 'styles/globals.css';
+import type { AppProps } from 'next/app';
 
 function MyApp({ Component, pageProps }: AppProps) {
     return <Component {...pageProps} />;
