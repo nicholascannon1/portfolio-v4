d12d21fca1f11075816ea0a93adb20a8ac2e6f17
nicholascannon <nicholascannon1@gmail.com>
2024-08-14 14:32:04 +1000

    Add eslint import stuff

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,14 +7,42 @@ module.exports = {
         'plugin:@typescript-eslint/recommended',
         'plugin:react-hooks/recommended',
         'plugin:prettier/recommended',
+        'plugin:import/recommended',
+        'plugin:import/typescript',
     ],
     ignorePatterns: ['dist', '.eslintrc.cjs'],
     parser: '@typescript-eslint/parser',
-    plugins: ['react-refresh'],
+    plugins: ['react-refresh', 'unused-imports'],
     rules: {
         'react-refresh/only-export-components': [
             'warn',
             { allowConstantExport: true },
+        ],
+
+        // https://github.com/import-js/eslint-plugin-import/blob/main/docs/rules/order.md
+        'import/order': [
+            'error',
+            {
+                alphabetize: {
+                    order: 'asc',
+                },
+                'newlines-between': 'never',
+                groups: ['builtin', 'external', 'parent', 'sibling', 'index'],
+            },
+        ],
+
+        // Allow variables prefixed with underscore to be unused
+        'no-unused-vars': 'off',
+        '@typescript-eslint/no-unused-vars': 'off',
+        'unused-imports/no-unused-imports': 'error',
+        'unused-imports/no-unused-vars': [
+            'warn',
+            {
+                vars: 'all',
+                varsIgnorePattern: '^_',
+                args: 'after-used',
+                argsIgnorePattern: '^_',
+            },
         ],
     },
 };
